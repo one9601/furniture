fe244199a9771f4cf65bb88532b375f6598583ac
alswjd1125 <alslal1125@naver.com>
2020-10-27 15:38:22 +0900

    fatch
    fatch

---
+++ js/buy-modal.js
@@ -0,0 +1,9 @@
+    $(function(){
+        $('#modal-btn').click(function(){
+            $('#myModal').show();
+        });
+    });
+    //팝업 close 가능
+    function close_pop(flag){
+        $("#myModal").hide();
+    };
