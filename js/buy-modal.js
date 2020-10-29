96e9f377b36a5c6051cdbfdca86dc6ace30c12c0
alswjd1125 <alslal1125@naver.com>
2020-10-29 18:21:06 +0900

    delete
    delete

--- js/buy-modal.js
+++
@@ -1,9 +0,0 @@
-    $(function(){
-        $('#modal-btn').click(function(){
-            $('#myModal').show();
-        });
-    });
-    //팝업 close 가능
-    function close_pop(flag){
-        $("#myModal").hide();
-    };
