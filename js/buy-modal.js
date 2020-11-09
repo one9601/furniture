bbd7b804e4ade8394871f3fa20ed6e9a5b5ebb91
alswjd1125 <alslal1125@naver.com>
2020-11-09 09:31:48 +0900

    fetch
    fetch

--- js/buy-modal.js
+++ js/buy-modal.js
@@ -7,3 +7,14 @@
     function close_pop(flag){
         $("#myModal").hide();
     };
+
+    $(function(){
+        $('#modal-btn-bottom').click(function(){
+            $('#myModal2').show();
+        });
+    });
+    //팝업 close 가능
+    function close_pop2(flag){
+        $("#myModal2").hide();
+    };
+    
